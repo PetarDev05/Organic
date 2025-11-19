03339a331f13e72f19c0d6dd657d44b65903b4fa
PetarPejovic <pejovicpetar02@gmail.com>
2025-11-19 09:51:31 +0100

    Make schema and model for products

---
+++ server/src/models/products.models.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const productsSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    about: [{ type: String, required: true }],
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productsSchema);
+
+export default Product;
